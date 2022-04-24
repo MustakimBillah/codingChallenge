adf0ce87768692d50a985f189dcabb83864c29f6
Mustakim Billah <mustakim.billah@revesoft.com>
2022-04-24 23:12:49 +0600

    Project Initialized

---
+++ src/test/java/com/mustakim/codingchallenge/CodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustakim.codingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
